6cd177217e52e473bcd17add454f4b4762d9635c
soldiervinz <vinzenz.uhr@gmail.com>
2024-03-12 13:43:29 +0100

    2nd training iteration

---
+++ 06_Trainings/02 unconditional with accelerate/lesion_filling_unconditioned.py
@@ -0,0 +1,544 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import torch
+from torch.utils.tensorboard import SummaryWriter
+tb_summary = SummaryWriter("./Tensorboards/", purge_step=0)
+
+
+# In[2]:
+
+
+#create config
+from dataclasses import dataclass
+
+@dataclass
+class TrainingConfig:
+    image_size = 256  # TODO: the generated image resolution
+    channels = 1
+    train_batch_size = 4 # 16
+    eval_batch_size = 16  # how many images to sample during evaluation
+    num_epochs = 100
+    gradient_accumulation_steps = 1
+    learning_rate = 1e-4
+    lr_warmup_steps = 100 #500
+    save_image_epochs = 3
+    save_model_epochs = 10
+    mixed_precision = "fp16"  # `no` for float32, `fp16` for automatic mixed precision
+    output_dir = "lesion-filling-256_2"  # the model name locally and on the HF Hub
+    dataset_path = "./dataset/imgs"
+    segm_path = "./dataset/segm"
+    num_gpu=2
+    #uniform_dataset_path = "./uniform_dataset"
+
+    push_to_hub = False  # whether to upload the saved model to the HF Hub
+    #hub_model_id = "<your-username>/<my-awesome-model>"  # the name of the repository to create on the HF Hub
+    #hub_private_repo = False
+    #overwrite_output_dir = True  # overwrite the old model when re-running the notebook
+    seed = 0
+config = TrainingConfig()
+
+
+# In[3]:
+
+
+#log at tensorboard
+tb_summary.add_scalar("image_size", config.image_size, 0)
+tb_summary.add_scalar("train_batch_size", config.train_batch_size, 0)
+tb_summary.add_scalar("eval_batch_size", config.eval_batch_size, 0)
+tb_summary.add_scalar("num_epochs", config.num_epochs, 0)
+tb_summary.add_scalar("learning_rate", config.learning_rate, 0)
+tb_summary.add_scalar("lr_warmup_steps", config.lr_warmup_steps, 0)
+tb_summary.add_scalar("save_image_epochs", config.save_image_epochs, 0)
+tb_summary.add_text("mixed_precision", config.mixed_precision, 0) 
+
+
+# In[4]:
+
+
+#Preprocess with UniRes
+"""
+from unires.struct import settings
+from unires.run import preproc
+import os
+from pathlib import Path
+
+reconstructed_affine_matrices = dict()
+
+os.makedirs(config.uniform_dataset_path, exist_ok=True)
+if (not any(Path(config.uniform_dataset_path).iterdir())):
+    data = list(Path(config.dataset_path).rglob("*.nii.gz"))
+    [str(x) for x in data]
+    s = settings()
+    s.common_output = True  # ensures 'standardised' outputs across subjects
+    s.dir_out = config.uniform_dataset_path
+    for img in data:
+        _, mat, uniform_data_pth = preproc(img, sett=s)
+        reconstructed_affine_matrices[uniform_data_pth] = mat 
+else:
+    print("Skipping preprocessing with UniRes, because output_dir is not empty")
+"""
+
+
+# In[5]:
+
+
+#Preprocess with Nitorch
+""" 
+import os
+import nitorch as ni
+from nitorch.tools.preproc import atlas_align
+from pathlib import Path
+
+reconstructed_affine_matrices = dict()
+
+os.makedirs(config.uniform_dataset_path, exist_ok=True)
+if (not any(Path(config.uniform_dataset_path).iterdir())):
+    data = list(Path(config.dataset_path).rglob("*.nii.gz"))
+    [str(x) for x in data]
+
+    for img in data[0:10]:
+        nii_mov = ni.io.map(img)
+        dat_mov = nii_mov.fdata(device=device)
+        mat_mov = nii_mov.affine.to(device)
+    
+        dat_mov_atlas, mat, pth, _ = atlas_align([dat_mov, mat_mov], rigid=False, device=device, default_atlas="atlas_t1", write="affine", odir=config.uniform_dataset_path)
+        reconstructed_affine_matrices[pth] = mat
+
+    #
+    #dat_mov = list()
+    #mat_mov = list()
+    #for img in data[0:10]:
+    #    nii_mov = ni.io.map(img)
+    #    dat_mov.append(nii_mov.fdata(device=device))
+    #    mat_mov.append(nii_mov.affine.to(device))
+
+    #dat_mov_atlas, mat, paths, _ = atlas_align([list(x) for x in zip(dat_mov, mat_mov)], rigid=False, device=device, default_atlas="atlas_t1", write="affine", odir=config.uniform_dataset_path)
+
+    #for i, path in enumerate(paths):
+    #    reconstructed_affine_matrices[path] = mat[i]
+    
+else:
+    print("Skipping preprocessing with Nitorch, because output_dir is not empty")
+"""
+
+
+# In[6]:
+
+
+# create dataset
+from torch.utils.data import Dataset
+from torch.nn import functional as F
+from pathlib import Path
+import nibabel as nib
+import numpy as np
+from math import floor, ceil
+
+class Dataset_Training(Dataset):
+    """
+    Dataset for Training purposes. 
+    Adapted implementation of BraTS 2023 Inpainting Challenge (https://github.com/BraTS-inpainting/2023_challenge).
+    
+    Contains ground truth t1n images (gt) 
+    Args:
+        root_dir: Path to dataset files
+        pad_shape: Shape the images will be transformed to
+        transversal_range: Range of transversal 2D slices which should be considered for training
+
+    Raises:
+        UserWarning: When your input images are not (256, 256, 160)
+
+    Returns: 
+        __getitem__: Returns a dictoinary containing:
+            "gt_image": Padded and cropped version of t1n 2D slice
+            "t1n_path": Path to the unpadded t1n file for this sample
+            "max_v": Maximal value of t1 image (used for normalization) 
+    """
+
+    def __init__(self, root_dir_img: Path, root_dir_segm: Path, pad_shape=(256,256,256)): # TODO: better word for transversal_range? #Todo: document root dirs, 
+        #Initialize variables
+        self.root_dir_img = root_dir_img
+        self.root_dir_segm = root_dir_segm
+        self.pad_shape = pad_shape 
+        bottom_offset=60 
+        top_offset=20
+
+        # Ground truth specific paths
+        self.list_paths_t1n = list(root_dir_img.rglob("*.nii.gz"))
+        self.list_paths_segm = list(root_dir_segm.rglob("*.nii.gz"))
+
+        idx=0
+        self.idx_to_2D_slice = dict()
+        for j, path in enumerate(self.list_paths_segm):
+            t1n_segm = nib.load(path)
+            t1n_3d = t1n_segm.get_fdata()
+ 
+            i=0
+            while(not t1n_3d[:,i,:].any()):
+                i+=1
+            bottom=i+bottom_offset
+            
+            i=t1n_3d.shape[1]-1
+            while(not t1n_3d[:,i,:].any()):
+                i-=1
+            top=i-top_offset
+
+            for i in np.arange(top-bottom):
+                self.idx_to_2D_slice[idx]=(self.list_paths_t1n[j],bottom+i)
+                idx+=1 
+
+    def __len__(self): 
+        return len(self.idx_to_2D_slice.keys()) 
+
+    def preprocess(self, t1n: np.ndarray):
+        """
+        Transforms the images to a more unified format.
+        Normalizes to -1,1. Pad and crop to bounding box.
+        
+        Args:
+            t1n (np.ndarray): t1n from t1n file (ground truth).
+
+        Raises:
+            UserWarning: When your input images are not (256, 256, 160)
+
+        Returns:
+            t1n: The padded and cropped version of t1n.
+            t1n_max_v: Maximal value of t1n image (used for normalization).
+        """
+
+        #Size assertions
+        reference_shape = (256,256,160)
+        if t1n.shape != reference_shape:
+            raise UserWarning(f"Your t1n shape is not {reference_shape}, it is {t1n.shape}")
+
+        #Normalize the image to [0,1]
+        t1n[t1n<0] = 0 #Values below 0 are considered to be noise #TODO: Check validity
+        t1n_max_v = np.max(t1n)
+        t1n /= t1n_max_v
+
+        #pad to bounding box
+        size = self.pad_shape # shape of bounding box is (size,size,size) #TODO: Find solution for 2D
+        t1n = torch.Tensor(t1n)
+        d, w, h = t1n.shape[-3], t1n.shape[-2], t1n.shape[-1]
+        d_max, w_max, h_max = size
+        d_pad = max((d_max - d) / 2, 0)
+        w_pad = max((w_max - w) / 2, 0)
+        h_pad = max((h_max - h) / 2, 0)
+        padding = (
+            int(floor(h_pad)),
+            int(ceil(h_pad)),
+            int(floor(w_pad)),
+            int(ceil(w_pad)),
+            int(floor(d_pad)),
+            int(ceil(d_pad)),
+        )
+        t1n = F.pad(t1n, padding, value=0, mode="constant") 
+
+        #map images from [0,1] to [-1,1]
+        t1n = (t1n*2) - 1
+
+        return t1n, t1n_max_v
+
+    def __getitem__(self, idx):
+        t1n_path = self.idx_to_2D_slice[idx][0]
+        slice_idx = self.idx_to_2D_slice[idx][1]
+        t1n_img = nib.load(t1n_path) # around 0.6s on local machine
+        t1n = t1n_img.get_fdata()
+        
+        # preprocess data
+        t1n, t1n_max_v = self.preprocess(t1n) # around 0.2s on local machine
+        
+        # get 2D slice from 3D
+        t1n_slice = t1n[:,slice_idx,:] 
+        
+        #t1n_slice = t1n[:128,slice_idx,:128] 
+
+        
+        # Output data
+        sample_dict = {
+            "gt_image": t1n_slice.unsqueeze(0),
+            "t1n_path": str(t1n_path),  # path to the 3D t1n file for this sample.
+            "max_v": t1n_max_v,  # maximal t1n_voided value used for normalization 
+        }
+        return sample_dict 
+
+
+# In[7]:
+
+
+#create dataset
+datasetTrain = Dataset_Training(Path(config.dataset_path), Path(config.segm_path), pad_shape=(256, 256, 256)) # TODO: check shape
+
+print(f"Dataset size: {len(datasetTrain)}")
+print(f"\tImage shape: {datasetTrain[0]['gt_image'].shape}")
+print(f"Training Data: {list(datasetTrain[0].keys())}") 
+
+
+# In[8]:
+
+
+list_paths_t1n = list(Path(config.dataset_path).rglob("*.nii.gz"))
+list_paths_segm = list(Path(config.segm_path).rglob("*.nii.gz"))
+
+
+# In[9]:
+
+
+# plot random image
+import matplotlib.pyplot as plt
+
+# Get 8 random sample
+random_indices = np.random.randint(0, len(datasetTrain) - 1, size=(8)) 
+
+# Plot: t1n
+fig, axis = plt.subplots(2,4,figsize=(16,4))
+for i, idx in enumerate(random_indices):
+    axis[i//4,i%4].imshow((datasetTrain[idx]["gt_image"].squeeze()+1)/2)
+    axis[i//4,i%4].set_axis_off()
+fig.show()
+
+
+# In[10]:
+
+
+from torch.utils.data import DataLoader
+
+
+train_dataloader = DataLoader(datasetTrain, batch_size=config.train_batch_size, shuffle=True)
+
+
+# In[11]:
+
+
+#create model
+from diffusers import UNet2DModel
+
+model = UNet2DModel(
+    sample_size=config.image_size,  # the target image resolution
+    in_channels=config.channels,  # the number of input channels, 3 for RGB images
+    out_channels=config.channels,  # the number of output channels
+    layers_per_block=2,  # how many ResNet layers to use per UNet block
+    block_out_channels=(128, 128, 256, 256, 512, 512),  # the number of output channels for each UNet block
+    down_block_types=(
+        "DownBlock2D",  # a regular ResNet downsampling block
+        "DownBlock2D",
+        "DownBlock2D",
+        "DownBlock2D",
+        "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
+        "DownBlock2D",
+    ),
+    up_block_types=(
+        "UpBlock2D",  # a regular ResNet upsampling block
+        "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
+        "UpBlock2D",
+        "UpBlock2D",
+        "UpBlock2D",
+        "UpBlock2D",
+    ),
+)
+
+
+tb_summary.add_text("model", "UNet2DModel", 0) 
+
+
+# In[12]:
+
+
+#check if image size matches
+sample_image = datasetTrain[0]['gt_image'].unsqueeze(0)
+print("Input shape:", sample_image.shape)
+
+print("Output shape:", model(sample_image, timestep=0).sample.shape)
+
+
+# In[13]:
+
+
+#setup noise scheduler
+import torch
+from PIL import Image
+from diffusers import DDIMScheduler
+
+noise_scheduler = DDIMScheduler(num_train_timesteps=1000)
+noise = torch.randn(sample_image.shape)
+timesteps = torch.LongTensor([50])
+noisy_image = noise_scheduler.add_noise(sample_image, noise, timesteps)
+
+tb_summary.add_text("noise_scheduler", "DDIMScheduler(num_train_timesteps=1000)", 0) 
+
+#Image.fromarray(((noisy_image.permute(0, 2, 3, 1) + 1.0) * 127.5).type(torch.uint8).numpy()[0])
+
+
+
+
+# In[14]:
+
+
+# setup lr scheduler
+from diffusers.optimization import get_cosine_schedule_with_warmup
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=config.learning_rate)
+lr_scheduler = get_cosine_schedule_with_warmup(
+    optimizer=optimizer,
+    num_warmup_steps=config.lr_warmup_steps,
+    num_training_steps=(len(train_dataloader) * config.num_epochs),
+)
+
+tb_summary.add_text("lr_scheduler", "cosine_schedule_with_warmup", 0) 
+
+
+# In[15]:
+
+
+#setup evaluation
+from diffusers import DDIMPipeline
+from diffusers.utils import make_image_grid
+import os
+
+def evaluate(config, epoch, pipeline):
+    # Sample some images from random noise (this is the backward diffusion process).
+    # The default pipeline output type is `List[PIL.Image]`
+    model.eval()
+    images = pipeline(
+        batch_size=config.eval_batch_size,
+        generator=torch.manual_seed(config.seed),
+    ).images 
+    # Make a grid out of the images
+    image_grid = make_image_grid(images, rows=4, cols=4)
+
+    # Save the images
+    test_dir = os.path.join(config.output_dir, "samples")
+    os.makedirs(test_dir, exist_ok=True)
+    image_grid.save(f"{test_dir}/{epoch:04d}.png")
+    print("image saved")
+
+
+# In[21]:
+
+
+#from accelerate import Accelerator
+from accelerate import Accelerator
+from huggingface_hub import create_repo, upload_folder
+from tqdm.auto import tqdm
+from pathlib import Path
+import os 
+import torch.nn as nn 
+import torch.nn.functional as F
+import sys
+import time
+
+def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler):
+    
+    accelerator = Accelerator(
+        mixed_precision=config.mixed_precision,
+        gradient_accumulation_steps=config.gradient_accumulation_steps,  
+    )
+
+    if accelerator.is_main_process:
+        if config.output_dir is not None:
+            os.makedirs(config.output_dir, exist_ok=True) 
+        accelerator.init_trackers("train_example")
+
+    model, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
+        model, optimizer, train_dataloader, lr_scheduler
+    )
+    
+    os.makedirs(config.output_dir, exist_ok=True)  
+
+    global_step = 0
+
+    
+    
+    # Now you train the model
+    for epoch in range(config.num_epochs): 
+        model.train()
+        #progress_bar = tqdm(total=len(train_dataloader), disable=not accelerator.is_local_main_process)
+        progress_bar = tqdm(total=len(train_dataloader))
+        progress_bar.set_description(f"Epoch {epoch}") 
+        
+        for step, batch in enumerate(train_dataloader): 
+            
+            clean_images = batch["gt_image"]
+            clean_images = clean_images 
+            
+            # Sample noise to add to the images
+            noise = torch.randn(clean_images.shape, device=clean_images.device)
+            bs = clean_images.shape[0]
+
+            # Sample a random timestep for each image
+            timesteps = torch.randint(
+                0, noise_scheduler.config.num_train_timesteps, (bs,), device=clean_images.device,
+                dtype=torch.int64
+            )
+
+            # Add noise to the clean images according to the noise magnitude at each timestep
+            # (this is the forward diffusion process)
+            noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
+
+            with accelerator.accumulate(model):
+                # Predict the noise residual
+                noise_pred = model(noisy_images, timesteps, return_dict=False)[0]
+                loss = F.mse_loss(noise_pred, noise)
+                accelerator.backward(loss)
+                #loss.backward()
+                accelerator.clip_grad_norm_(model.parameters(), 1.0)
+                #nn.utils.clip_grad_value_(model.parameters(),1.0)
+    
+                #log gradient norm 
+                parameters = [p for p in model.parameters() if p.grad is not None and p.requires_grad]
+                if len(parameters) == 0:
+                    total_norm = 0.0
+                else: 
+                    total_norm = torch.norm(torch.stack([torch.norm(p.grad.detach()).cpu() for p in parameters]), 2.0).item()
+                
+                optimizer.step()
+                lr_scheduler.step()
+                optimizer.zero_grad()
+
+            progress_bar.update(1)            
+            logs = {"loss": loss.cpu().detach().item(), "lr": lr_scheduler.get_last_lr()[0], "total_norm": total_norm, "step": global_step}
+            tb_summary.add_scalar("loss", logs["loss"], global_step)
+            tb_summary.add_scalar("lr", logs["lr"], global_step) 
+            tb_summary.add_scalar("total_norm", logs["total_norm"], global_step) 
+            
+            progress_bar.set_postfix(**logs)
+            accelerator.log(logs, step=global_step)
+            global_step += 1 
+
+        # After each epoch you optionally sample some demo images with evaluate() and save the model
+        if accelerator.is_main_process:
+            #pipeline = DDPMPipeline(unet=accelerator.unwrap_model(model), scheduler=noise_scheduler)
+            pipeline = DDIMPipeline(unet=model, scheduler=noise_scheduler) 
+    
+            if (epoch) % config.save_image_epochs == 0 or epoch == config.num_epochs - 1:
+                evaluate(config, epoch, pipeline)
+    
+            if (epoch) % config.save_model_epochs == 0 or epoch == config.num_epochs - 1: 
+                pipeline.save_pretrained(config.output_dir)
+
+
+tb_summary.add_text("inference_pipeline", "DDIMPipeline", 0) 
+
+
+# In[ ]:
+
+
+from accelerate import notebook_launcher
+
+args = (config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler)
+
+notebook_launcher(train_loop, args, num_processes=1)
+
+#train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler)    
+
+
+# In[ ]:
+
+
+#create python script for ubelix
+get_ipython().system('jupyter nbconvert --to script "lesion_filling_unconditioned.ipynb"')
+#adjust batch size
+
