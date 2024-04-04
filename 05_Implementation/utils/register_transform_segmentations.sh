99f86fecd05b74b7cc6ffef61ed1e71e37a7673f
soldiervinz <vinzenz.uhr@gmail.com>
2024-04-04 11:04:09 +0200

    remove old code

--- 05_Implementation/utils/register_transform_segmentations.sh
+++ 05_Implementation/utils/register_transform_segmentations.sh
@@ -14,59 +14,39 @@ temp_path="/home/jovyan/workdir/Master_Thesis/Diffusion_Lesions/temp_04_Datasets
 mkdir -p $output_path
 mkdir -p $temp_path
 
-#Register and transform FLAIR segmentation to T1
-#for float_folder in $float_mri_path"/*"
-#do
-#	mkdir -p $output_path"/${ref_folder##*/}/${float_folder##*/}/"
-#	#Affine registration
-#	$nifty_path"/reg_aladin" -ref $float_folder"/3DT1.nii.gz" -flo $float_folder/$float_flair_name-aff $temp_path"/temp_aff_transform.txt" -res $temp_path"/temp_img.nii.gz"
-#	#Non linear registration with affine initialization 
-#	$nifty_path"/reg_f3d" -ref $float_folder"/3DT1.nii.gz" -flo $float_folder/$float_flair_name -aff $temp_path"/temp_aff_transform.txt" -res $temp_path"/temp_img.nii.gz" -cpp $temp_path"/temp_non_linear_transform.nii"
-#	#Transformation based on non linear registration. Inter=0 for nearest neighbor interpolation
-#	$nifty_path"/reg_resample" -ref $float_folder"/3DT1.nii.gz" -flo $float_folder"/Consensus.nii.gz" -trans $temp_path"/temp_non_linear_transform.nii" -res $float_folder"/Consensus_T1registered.nii.gz" -inter 0
-#done
-#test for 1 patient with 1 image
-#mkdir -p $output_path"/${ref_folder##*/}/${float_folder##*/}/"
-#$nifty_path"/reg_aladin" -ref $float_folder"/3DT1.nii.gz" -flo $float_folder/$float_flair_name -aff $temp_path"/temp_aff_transform.txt" -res $temp_path"/temp_img.nii.gz"
-#$nifty_path"/reg_f3d" -ref $float_folder"/3DT1.nii.gz" -flo $float_folder/$float_flair_name -aff $temp_path"/temp_aff_transform.txt" -res $temp_path"/temp_img.nii.gz" -cpp $temp_path"/temp_non_linear_transform.nii"
-#$nifty_path"/reg_resample" -ref $float_folder"/3DT1.nii.gz" -flo $float_folder"/Consensus.nii.gz" -trans $temp_path"/temp_non_linear_transform.nii" -res $float_folder"/Consensus_T1registered.nii.gz" -inter 0
-
-#$nifty_path"/reg_aladin" -ref $ref_folder/$ref_file_name -flo $float_folder"/3DT1.nii.gz" -aff $temp_path"/temp_aff_transform.txt" -res $temp_path"/temp_img.nii.gz"
-#$nifty_path"/reg_f3d" -ref $ref_folder/$ref_file_name -flo $float_folder"/3DT1.nii.gz" -aff $temp_path"/temp_aff_transform.txt" -res $temp_path"/temp_img.nii.gz" -cpp $temp_path"/temp_non_linear_transform.nii"
-#$nifty_path"/reg_resample" -ref $ref_folder/$ref_file_name -flo $float_folder"/Consensus_T1registered.nii.gz" -trans $temp_path"/temp_non_linear_transform.nii" -res $output_path"/${ref_folder##*/}/${float_folder##*/}/transformed_segmentation.nii.gz" -inter 0
-	
-#Register and transform segmentations between different patients
-#for ref_folder in $ref_mri_path
-#do
-#	for float_folder in $float_mri_path
-#	do  
-#		
-#		mkdir -p $output_path"/${ref_folder##*/}/${float_folder##*/}/"
-#		#Affine registration  
-#		"${nifty_path}/reg_aladin" -ref $ref_folder/$ref_file_name -flo $float_folder/$float_flair_name -aff $temp_path/temp_aff_transform.txt -res $temp_path/temp_img.nii.gz
-#		#Non linear registration with affine initialization
-#		"${nifty_path}/reg_f3d" -ref $ref_folder/$ref_file_name -flo $float_folder/$float_flair_name -aff $temp_path"/temp_aff_transform.txt" -res $temp_path"/temp_img.nii.gz" -cpp $temp_path"/temp_non_linear_transform.nii"
-#		#Transformation based on non linear registration. Inter=0 for nearest neighbor interpolation
-#		"${nifty_path}/reg_resample" -ref $ref_folder/$ref_file_name -flo $float_folder/$float_segmentation_name -trans $temp_path"/temp_non_linear_transform.nii" -res $output_path"/${ref_folder##*/}/${float_folder##*/}/transformed_segmentation.nii.gz" -inter 0
-#	done
-#done
-
+Register and transform segmentations between different patients
 for ref_folder in $ref_mri_path
 do
 	for float_folder in $float_mri_path
 	do  
+		
 		mkdir -p $output_path"/${ref_folder##*/}/${float_folder##*/}/"
 		#Affine registration  
 		"${nifty_path}/reg_aladin" -ref $ref_folder/$ref_file_name -flo $float_folder/$float_flair_name -aff $temp_path/temp_aff_transform.txt -res $temp_path/temp_img.nii.gz
 		#Non linear registration with affine initialization
 		"${nifty_path}/reg_f3d" -ref $ref_folder/$ref_file_name -flo $float_folder/$float_flair_name -aff $temp_path"/temp_aff_transform.txt" -res $temp_path"/temp_img.nii.gz" -cpp $temp_path"/temp_non_linear_transform.nii"
-		for segmentation_file in $float_folder/ManualSegmentation*.nii.gz
-		do
-			#Transformation based on non linear registration. Inter=0 for nearest neighbor interpolation
-			"${nifty_path}/reg_resample" -ref $ref_folder/$ref_file_name -flo $segmentation_file -trans $temp_path"/temp_non_linear_transform.nii" -res $output_path"/${ref_folder##*/}/${float_folder##*/}/${segmentation_file##*/}" -inter 0
-		done
+		#Transformation based on non linear registration. Inter=0 for nearest neighbor interpolation
+		"${nifty_path}/reg_resample" -ref $ref_folder/$ref_file_name -flo $float_folder/$float_segmentation_name -trans $temp_path"/temp_non_linear_transform.nii" -res $output_path"/${ref_folder##*/}/${float_folder##*/}/transformed_segmentation.nii.gz" -inter 0
 	done
 done
+
+#If there are multiple floating objects to be transformed for one registration
+#for ref_folder in $ref_mri_path
+#do
+#	for float_folder in $float_mri_path
+#	do  
+#		mkdir -p $output_path"/${ref_folder##*/}/${float_folder##*/}/"
+#		#Affine registration  
+#		"${nifty_path}/reg_aladin" -ref $ref_folder/$ref_file_name -flo $float_folder/$float_flair_name -aff $temp_path/temp_aff_transform.txt -res $temp_path/temp_img.nii.gz
+#		#Non linear registration with affine initialization
+#		"${nifty_path}/reg_f3d" -ref $ref_folder/$ref_file_name -flo $float_folder/$float_flair_name -aff $temp_path"/temp_aff_transform.txt" -res $temp_path"/temp_img.nii.gz" -cpp $temp_path"/temp_non_linear_transform.nii"
+#		for segmentation_file in $float_folder/ManualSegmentation*.nii.gz
+#		do
+#			#Transformation based on non linear registration. Inter=0 for nearest neighbor interpolation
+#			"${nifty_path}/reg_resample" -ref $ref_folder/$ref_file_name -flo $segmentation_file -trans $temp_path"/temp_non_linear_transform.nii" -res $output_path"/${ref_folder##*/}/${float_folder##*/}/${segmentation_file##*/}" -inter 0
+#		done
+#	done
+#done
 
 rm $temp_path"/temp_img.nii.gz"
 rm $temp_path"/temp_non_linear_transform.nii"
