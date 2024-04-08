18517d2c3f56fd24341bac634e6d19aaf92b0dfb
soldiervinz <vinzenz.uhr@gmail.com>
2024-04-08 17:47:59 +0200

    add white matter lesions based on gray matter values

--- 05_Implementation/Jupyter notebooks/custom_modules/Evaluation2DSynthesis.py
+++ 05_Implementation/Jupyter notebooks/custom_modules/Evaluation2DSynthesis.py
@@ -1,9 +1,11 @@
 from Evaluation2D import Evaluation2D
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Evaluation2DSynthesis(Evaluation2D):
     def __init__(self, config, pipeline, dataloader, tb_summary, accelerator, train_env):
+        assert type(pipeline).__name__ == "DDIMGuidedPipeline", "Pipeline must be of type DDIMGuidedPipeline"
         super().__init__(config, pipeline, dataloader, tb_summary, accelerator, train_env)
 
     def _add_lesions(self, clean_images, masks):
@@ -11,15 +13,34 @@ class Evaluation2DSynthesis(Evaluation2D):
         images_with_lesions = clean_images.clone()
         images_with_lesions[masks] = -1
         return images_with_lesions
+    
+    def _add_lesions_from_gm(self, clean_images, masks, segmentation):
+        segmentation = segmentation.unsqueeze(0) 
+        assert clean_images.shape == segmentation.shape, "clean_images and segmentation must have the same shape"
 
-    def _start_pipeline(self, clean_images, masks, parameters):
+        #get mean value of gray matter
+        gm_labels = torch.cat([torch.arange(1000,1040), torch.arange(2000,2040)])
+        gm_mask = torch.isin(segmentation, gm_labels) 
+        gm_mean = clean_images[gm_mask].mean()
+        gm_std = clean_images[gm_mask].std()
+
+        #add white matter lesions with mean value of gray matter minus three times the standard deviation
+        print("gm_mean: ", gm_mean)
+        print("gm_std: ", gm_std)
+        print("new value: ", gm_mean - 3*gm_std)
+        images_with_lesions = clean_images.clone()
+        images_with_lesions[masks.to(torch.bool)] = gm_mean - 3*gm_std
+        return images_with_lesions
+
+    def _start_pipeline(self, clean_images, masks, segmentation, parameters={}):  
         #add coarse lesions
-        images_with_lesions = self._add_lesions(clean_images, masks)
+        images_with_lesions = self._add_lesions_from_gm(clean_images, masks, segmentation)
 
+        #TODO: add intermediate timestep
         #run it through network
         synthesized_images = self.pipeline(
             images_with_lesions,
-            masks,
+            timestep=25,
             generator=torch.cuda.manual_seed_all(self.config.seed),
             output_type=np.array,
             num_inference_steps = self.config.num_inference_steps,
