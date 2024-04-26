cfd7a3d634c469ab54a020cbe14299c234d12c7c
soldiervinz <vinzenz.uhr@gmail.com>
2024-04-26 14:21:26 +0200

    added guidedrepaint pipeline to pipelinefactories

--- 05_Implementation/Jupyter notebooks/custom_modules/PipelineFactories.py
+++ 05_Implementation/Jupyter notebooks/custom_modules/PipelineFactories.py
@@ -2,6 +2,7 @@ from RePaintPipeline import RePaintPipeline
 from DDIMInpaintPipeline import DDIMInpaintPipeline
 from DDIMGuidedPipeline import DDIMGuidedPipeline
 from diffusers import RePaintScheduler
+from GuidedRePaintPipeline import GuidedRePaintPipeline
 
 def get_ddim_guided_pipeline(model, noise_scheduler):
     return DDIMGuidedPipeline(unet=model, scheduler=noise_scheduler)
@@ -11,3 +12,6 @@ def get_ddim_inpaint_pipeline(model, noise_scheduler):
 
 def get_repaint_pipeline(model, noise_scheduler):
     return RePaintPipeline(unet=model, scheduler=RePaintScheduler())
+
+def get_guided_repaint_pipeline(model, noise_scheduler):
+    return GuidedRePaintPipeline(unet=model, scheduler=RePaintScheduler(), ddim_scheduler=noise_scheduler)
